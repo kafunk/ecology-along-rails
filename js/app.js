c83722febe09bfbe2602a4b85fbfa94cbbb96a2a
[kafunk] <[kafunk@uky.edu]>
2018-06-06 00:15:13 -0500

    upload more thorough script outline/pseudocode

--- js/app.js
+++ js/app.js
@@ -1,65 +1,91 @@
 (function () {
 
-  // INITIALIZE MAP
+// INITIALIZE MAP
 
-    // mapbox API access Token
-      L.mapbox.accessToken = 'pk.eyJ1Ijoia2FmdW5rIiwiYSI6ImNqYmc3dXJzczMzZWIzNHFmcmZuNjY3azMifQ.9i48EOQl4WCGZQqKRvuc_g';
+  // mapbox API access Token
+    L.mapbox.accessToken = 'pk.eyJ1Ijoia2FmdW5rIiwiYSI6ImNqYmc3dXJzczMzZWIzNHFmcmZuNjY3azMifQ.9i48EOQl4WCGZQqKRvuc_g';
 
-    // initialize map centered on conterminous US
-      var map = new mapboxgl.Map({
-          container: 'map',
-          style: 'mapbox://styles/kafunk/cjhzu1ypk2nhj2sn6qkg8e3x2',
-          center: [-96, 37.8],
-          maxBounds: L.latLngBounds([40, -89], [44, -86]),
-          zoom: 4,
-          minZoom: 3,
-          maxZoom: 18
-      });
-map.on('load', function() {
-    map.addLayer({
-      id: 'watersheds',
-      type: 'polygon',
-      source: {
-        type: 'geojson',
-        data: 'data/wbdhu10_extract.json'
-      },
-      paint: {
-        'polygon-color': [
-          'interpolate',
-          ['linear'],
-          ['number', ['get', getAttribute()]],
-          0, '#2DC4B2',
-          1, '#3BB3C3',
-          2, '#669EC4',
-          3, '#8B88B6',
-          4, '#A2719B',
-          5, '#AA5E79'
-        ],
-        'polygon-opacity': 0.8
-      }
-    }, 'huc10s');
-  });
+  // initialize map centered on conterminous US
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/kafunk/cjhzu1ypk2nhj2sn6qkg8e3x2',
+      center: [-96, 37.8],
+      maxBounds: L.latLngBounds([40, -89], [44, -86]),
+      zoom: 4,
+      minZoom: 3,
+      maxZoom: 18
+    });
+
+  // https://beta.observablehq.com/@mbostock/disposing-content
+  // try {
+  //   yield map;
+  //   yield invalidation;
+  // } finally {
+  //   map.remove();
+  // }
 
   // LOAD DATA
-    // use d3?
-    d3.json ('data/RAILROUTES')
-    d3.json ('data/railnodes')
-    d3.json ('data/huc10s')
-    d3.json ('data/ecopolys')
-    d3.json ('data/hydrolines')
-    d3.json ('data/otherlines')
-    d3.json ('data/ecopts')
-    d3.json ('data/otherpts')
-      .on('ready', function (e) {
-        // pass data as GeoJSON to drawMap function
-        // drawLegend(e.target.toGeoJSON());
-        drawMap(e.target.toGeoJSON());
+    var railwaysJson = d3.json("data/us-states.json"),
+      railnodesJson = d3.json(""),
+      huExtractJson = d3.json(""),
+      ecoRegJson = d3.json("")
+      // poisJson = d3.json("poiExtract.json")
+      // basemap: other rail, hydrolines, lrger watersheds,etc
+
+       // pois include eco, geo, tunnel, etc
+
+    // make sure both data files are completely loaded before sending data on to callback function
+    Promise.all([railwaysJson,railnodesJson,huExtractJson,ecoRegJson]).then(drawMap, error)
+
+    // catch errors, if any
+    function error(error) {
+      console.log(error)
+    }
+
+    // accepts the data array
+    function drawMap(data) {
+      console.log(data);
+      var railways = data[0],
+         railnodes = data[1],
+        watersheds = data[2],
+        ecoregions = data[3];
+
+      // define width and height of our SVG
+      var width = 960,
+         height = 600
+
+      // create svg element as child of #map element
+      var svg = d3.select("#map")
+        .append("svg")
+        .attr("width", width) // give the SVG element a width and height attributes and value
+        .attr("height", height)
+
+      // d3 feature method converts topoJSON back to geoJSON (convenient for most mapping applications)
+      var geojson = topojson.feature(statesData, {
+        type: "GeometryCollection",
+        geometries: statesData.objects.cb_2016_us_state_20m.geometries
       })
-      .on('error', function (e) {
-        console.log(e.error[0].message);
-      });
 
+      // define a projection using the US Albers USA
+        // fit the extent of the GeoJSON data to specified width and height
+      var projection = d3.geoAlbersUsa()
+        .fitSize([width, height], geojson)
 
+      // define a path generator, which will use the specified projection
+      var path = d3.geoPath()
+        .projection(projection)
+
+      // create and append a new SVG 'g' element to the SVG
+      var states = svg.append("g")
+        .selectAll("path")        // select all the paths (as yet non-existent)
+        .data(geojson.features)   // access the GeoJSON data
+        .enter()                  // enter the selection (all pre-selected path elements)
+        .append("path")           // create one new path element for each data feature
+        .attr("d", path)          // give each path a d attribute value
+        .attr("class", "state")   // give each path a class of state
+    }
+
+// GET USER SELECTION AND FILTER TO SINGLE ROUTE
   // variables to be populated dynamically pending on UI
     // start point
     var start = [-122.414, 37.776];
@@ -236,6 +262,21 @@ map.on('load', function() {
         counter = counter + 1;
     }
 
+    //animateLine
+    // http://bl.ocks.org/duopixel/4063326
+      // Variable to Hold Total Length
+      var totalLength = path.node().getTotalLength();
+
+      // Set Properties of Dash Array and Dash Offset and initiate Transition
+      path
+      	.attr("stroke-dasharray", totalLength + " " + totalLength)
+      	.attr("stroke-dashoffset", totalLength)
+        .transition() // Call Transition Method
+      	.duration(4000) // Set Duration timing (ms)
+      	.ease(d3.easeLinear) // Set Easing option
+      	.attr("stroke-dashoffset", 0); // Set final value of dash-offset for transition
+        }
+
 // DATA REVEALED AS ANIMATION PROGRESSES
   // Add and remove classes for dynamic styling
     // passed (cumulative/all), just_passed (radius=medium), now_passing (radius=small)
@@ -297,6 +338,17 @@ map.on('load', function() {
 
     // new features get extra special style and brief popup intro (?)
       // new_within_radius.style
+        // gradual fill of color/opacity?? https://stackoverflow.com/questions/17438409/color-transition-in-d3-using-time-elapsed#17439022
+
+        // https://datascience.blog.wzb.eu/2016/08/29/bringing-svg-to-life-with-d3-js/
+        // huc10s.each(function() {
+        //   d3.select(this).select('path')
+        //     .transition().duration(1000)
+        //     .style('fill', 'red')
+        // });
+        // cavnas + svg? to allow flood fill e.g.
+          // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Drawing_DOM_objects_into_a_canvas
+          // https://beta.observablehq.com/@mbostock/randomized-flood-fill
 
       // popup
         // popup.setLngLat(e.lngLat)
